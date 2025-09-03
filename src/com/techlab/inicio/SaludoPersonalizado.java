5682fe4036242966a08ac898a232401dfc5dac1b
GustavoVasquez <tavovm24@hotmail.com>
2025-09-03 12:14:19 -0300

    ejercicios clase 2 completado

--- src/com/techlab/inicio/SaludoPersonalizado.java
+++ src/com/techlab/inicio/SaludoPersonalizado.java
@@ -3,8 +3,5 @@ import java.util.Scanner;
 public class SaludoPersonalizado {
   public static void main(String[] args) {
     System.out.println("Benvenidos a mi proyeto ecomerce con JAVA");
-    Scanner sc = new Scanner(System.in); //Primero necesitamos crear un objeto Scanner
-    System.out.println("Ingrese su edad par saber si puede ingresar al bar:");
-    int edad = sc.nextInt();
   }
 }
--- src/com/techlab/inicio/clase2/bucles.java
+++ src/com/techlab/inicio/clase2/bucles.java
@@ -5,8 +5,15 @@ public class bucles {
     Scanner sc = new Scanner(System.in);
     System.out.println("Ingrese un número por favor");
     int num = sc.nextInt();
+    int cont=1;
+
     for(int i = 1; i <= num; i++){
       System.out.println(i);
+    }
+    //Repetir lo mismo pero con un while
+    while(cont <= num){
+      System.out.println(cont);
+      cont ++;
     }
   }
 }
