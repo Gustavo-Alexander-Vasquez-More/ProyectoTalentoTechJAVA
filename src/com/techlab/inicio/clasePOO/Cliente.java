97d5884275c967b32e84a7a96a11a9592dda9c97
GustavoVasquez <tavovm24@hotmail.com>
2025-09-11 20:28:38 -0300

    POO

--- src/com/techlab/inicio/clasePOO/Cliente.java
+++ src/com/techlab/inicio/clasePOO/Cliente.java
@@ -1,16 +1,27 @@
 package com.techlab.inicio.clasePOO;
-//Crea una clase Cliente con atributos nombre y email.
-//Instanciá un objeto Cliente y asígnale valores a sus atributos.
+//Creá una clase Cliente con atributos nombre y email.
 public class Cliente {
   //ATRIBUTOS
   String nombre;
   String email;
+  //Creá un constructor para Cliente que reciba el nombre y email
+  public Cliente(String nombre, String email){
+    this.nombre=nombre;
+    this.email=email;
+  }
 
   public static void main(String[] args) {
-    //Instanciamos un objeto cliente
-    Cliente cliente= new Cliente();
-    cliente.nombre="Gustavo";
-    cliente.email="tavovm24@hotmail.com";
-
+    //Instanciá un objeto Cliente y asígnale valores a sus atributos.
+    Cliente cliente1= new Cliente("Alex More", "alexmore@hotmail.com");
+    //Creá varios clientes con este constructor y mostralos por pantalla
+    Cliente cliente2= new Cliente("Juan Moreno López", "juanlopez@hotmail.com");
+    Cliente cliente3= new Cliente("María Torre", "mari_to24@hotmail.com");
+    Cliente cliente4= new Cliente("Alejandro Pérez", "aleLop@hotmail.com");
+    Cliente cliente5= new Cliente("Martin Cirio", "martin_cirio@hotmail.com");
+    System.out.println(cliente1.nombre + " " + cliente1.email);
+    System.out.println(cliente2.nombre + " " + cliente2.email);
+    System.out.println(cliente3.nombre + " " + cliente3.email);
+    System.out.println(cliente4.nombre + " " + cliente4.email);
+    System.out.println(cliente5.nombre + " " + cliente5.email);
   }
 }
--- src/com/techlab/inicio/clasePOO/Producto.java
+++ src/com/techlab/inicio/clasePOO/Producto.java
@@ -1,26 +1,40 @@
 package com.techlab.inicio.clasePOO;
 //En la clase Producto, agrega un método mostrarInformacion() que imprima nombre, precio y stock.
 public class Producto {
-  double precio;
   String nombre;
+  double precio;
   int stock;
 
-  //MÉTODOS
-  public void mostrarInformacion(){
-    System.out.println(nombre);
-    System.out.println(precio);
-    System.out.println(stock);
+  public Producto(String nombre, double precio, int stock){
+    this.nombre=nombre;
+    this.precio=precio;
+    this.stock=stock;
   }
-  public void calcularDescuento(){
 
+  //como mostrará información de un objeto es específico lo definimos como metodo de instancia
+  public void mostraInformacion(){
+    System.out.println("Producto:" + this.nombre + " Precio:$" + this.precio +" Cantidad:" + this.stock);
+  }
+  //Creá un método para calcular un descuento general para todos los productos de un 10%
+  //Esto sera un método estático ya que es general y no solo de un objeto
+  public static double calculoDescuento(double precio){
+    return precio - ((precio * 10)/100);
   }
 
   public static void main(String[] args) {
-    Producto producto=new Producto();
-    producto.nombre="Café Latte";
-    producto.precio=50.00;
-    producto.stock=5;
-    //LLAMAR AL MÉTODO DEL OBJETO PRODUCTO
-    producto.mostrarInformacion();
+
+    Producto producto1=new Producto("arroz",20.50,50);
+    producto1.mostraInformacion();
+    Producto producto2=new Producto("arveja",50.00,5);
+    producto2.mostraInformacion();
+    Producto producto3=new Producto("lenteja",10.00,10);
+    producto3.mostraInformacion();
+    Producto producto4=new Producto("atún",40.70,2);
+    producto4.mostraInformacion();
+
+    System.out.println("El nuevo precio del producto " + producto1.nombre + " al hacerle descuento del 10 % es de $" + calculoDescuento(producto1.precio)); //para el arroz
+    System.out.println("El nuevo precio del producto " + producto2.nombre + " al hacerle descuento del 10 % es de $" + calculoDescuento(producto2.precio)); // para la arveja
+    System.out.println("El nuevo precio del producto " + producto3.nombre + " al hacerle descuento del 10 % es de $" + calculoDescuento(producto3.precio)); // para la lenteja
+    System.out.println("El nuevo precio del producto " + producto4.nombre + " al hacerle descuento del 10 % es de $" + calculoDescuento(producto4.precio)); // para el atún
   }
 }
